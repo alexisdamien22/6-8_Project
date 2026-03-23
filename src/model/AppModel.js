5c56198f2e45ded17217496310e5cf695adc4cee
FamilyTsuki <alban.elie590@gmail.com>
2026-03-23 19:52:41 +0100

    main page

--- src/model/AppModel.js
+++ src/model/AppModel.js
@@ -3,14 +3,15 @@ class AppModel {
     this.activeChild = {
       name: "Léo",
       instrument: "Piano",
+
       sessions: [
         { day: "Lun", status: "done" },
         { day: "Mar", status: "done" },
         { day: "Mer", status: "todo" },
         { day: "Jeu", status: "waiting" },
-        { day: "Ven", status: "waiting" },
+        { day: "Ven", status: "todo" },
         { day: "Sam", status: "waiting" },
-        { day: "Dim", status: "waiting" },
+        { day: "Dim", status: "todo" },
       ],
     };
   }
--- src/view/AppView.js
+++ src/view/AppView.js
@@ -5,6 +5,8 @@ class AppView {
 
   renderHome(childData) {
     let pathHTML = "";
+    const joursFr = ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"];
+    let dayActuel = joursFr[new Date().getDay()];
 
     for (let i = 0; i < childData.sessions.length; i++) {
       let session = childData.sessions[i];
@@ -14,13 +16,24 @@ class AppView {
       if (i % 4 === 2) offset = 0;
       if (i % 4 === 3) offset = -40;
 
+      let mascotteHTML = "";
+      let haloHTML = "";
+
+      if (session.day === dayActuel) {
+        mascotteHTML = `<img src="public/assets/img/mascottes/camelion.png" class="mascotte-path">`;
+        haloHTML = `<div class="today-halo"></div>`;
+      }
+
       pathHTML += `
-                    <div class="path-step ${session.status}" style="transform: translateX(${offset}px)">
-                        <div class="path-button-container">
-                            <div class="path-dot-shadow"></div> <div class="path-dot"></div>       
-                        </div>
-                        <span class="path-label">${session.day}</span>
-                    </div>`;
+                <div class="path-step ${session.status}" style="transform: translateX(${offset}px)">
+                    <div class="path-button-container">
+                        ${haloHTML}
+                        ${mascotteHTML}
+                        <div class="path-dot-shadow"></div> 
+                        <div class="path-dot"></div>    
+                    </div>
+                    <span class="path-label">${session.day}</span>
+                </div>`;
     }
 
     this.app.innerHTML = `
@@ -30,5 +43,19 @@ class AppView {
                 </div>
             </div>
         `;
+
+    this.scrollToCurrentDay();
+  }
+
+  scrollToCurrentDay() {
+    const currentElement = this.app.querySelector(".mascotte-path");
+    if (currentElement) {
+      setTimeout(() => {
+        currentElement.scrollIntoView({
+          behavior: "smooth",
+          block: "center",
+        });
+      }, 100);
+    }
   }
 }
