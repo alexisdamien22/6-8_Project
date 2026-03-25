345a3c7651a6485587393617298ca56b7b49412d
FamilyTsuki <alban.elie590@gmail.com>
2026-03-25 12:41:41 +0100

    kill litle bug

--- src/model/AppModel.js
+++ src/model/AppModel.js
@@ -8,9 +8,9 @@ class AppModel {
         { day: "Lun", status: "done" },
         { day: "Mar", status: "done" },
         { day: "Mer", status: "todo" },
-        { day: "Jeu", status: "waiting" },
+        { day: "Jeu", status: "nothing" },
         { day: "Ven", status: "todo" },
-        { day: "Sam", status: "waiting" },
+        { day: "Sam", status: "nothing" },
         { day: "Dim", status: "todo" },
       ],
     };
