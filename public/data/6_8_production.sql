6e0c2086a160e698d7c10768fb5d8ebe797898ac
Alexis DAMIEN <alexisdamien@icloud.com>
2026-03-26 09:39:56 +0100

    fixed database name

--- public/data/6_8_production.sql
+++ public/data/6_8_production.sql
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `6/8_production`
+-- Database: `six_huit_production`
 --
 
 -- --------------------------------------------------------
